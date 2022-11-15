7f5c440d03db8c569207b94b845bbf8825d7ec37
Norman <whosnorm@gmail.com>
2022-11-15 02:33:50 -0500

    added heading to auth page

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,6 +1,7 @@
 import SignUpForm from "../components/SignUpForm"
 import LoginForm from "../components/LoginForm"
 import { useState } from "react";
+import {AiFillBug} from "react-icons/ai"
 import "./Authpage.css"
 
 const AuthPage = ({setUser}) => {
@@ -12,7 +13,7 @@ const AuthPage = ({setUser}) => {
 
     return(
         <div className="auth">
-            <h1>AuthPage</h1>
+            <h1><AiFillBug className='bug-icon'/> N.P Bug-Tracker</h1>
             {showSignUp ? <SignUpForm setUser={setUser} /> : <LoginForm setUser={setUser}/>}
 
             <button onClick={() => setShowSignUp(!showSignUp)}> 
